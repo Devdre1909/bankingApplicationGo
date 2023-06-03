eb5c559a6033c4e24500c3133d15a9a6906b7df1
Adegoke Temitope <adegoketemitope1909@gmail.com>
2023-06-03 12:41:33 +0100

    create go package, change server to mux

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bankApplicationGo.com/banking
+
+go 1.20
--- main.go
+++ main.go
@@ -1,53 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-type Customer struct {
-	Name string `json:"full_name" xml:"name"`
-	City string `json:"city" xml:"city"`
-}
+import "bankApplicationGo.com/banking/app"
 
 func main() {
-
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
-
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
-}
-
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{
-			Name: "Adegoke Temitope",
-			City: "Akure",
-		},
-		{
-			Name: "Toluwanimi Daniel",
-			City: "Akure",
-		},
-		{
-			Name: "Ayomide Akindehin",
-			City: "Ibadan",
-		},
-	}
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
-	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
-	}
-
+	app.Start()
 }
