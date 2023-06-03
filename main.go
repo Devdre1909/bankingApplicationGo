947aace6ff591536ace2f0d6eb5278f3e33b2534
Adegoke Temitope <adegoketemitope1909@gmail.com>
2023-06-03 09:23:22 +0100

    create customers resourse and data, use struct and json tags

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name string `json:"full_name"`
+	City string `json:"city"`
+}
+
 func main() {
 
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 
@@ -16,4 +23,25 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{
+			Name: "Adegoke Temitope",
+			City: "Akure",
+		},
+		{
+			Name: "Toluwanimi Daniel",
+			City: "Akure",
+		},
+		{
+			Name: "Ayomide Akindehin",
+			City: "Ibadan",
+		},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+
 }
