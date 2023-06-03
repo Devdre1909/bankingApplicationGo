7b0cf86374a9b4f25ffdbc0ea26909ff1b566410
Adegoke Temitope <adegoketemitope1909@gmail.com>
2023-06-03 09:11:40 +0100

    create basic server with resourse /greet

--- main.go
+++ main.go
@@ -1,2 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/greet", greet)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
